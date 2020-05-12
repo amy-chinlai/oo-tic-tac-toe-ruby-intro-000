d16583cc811ce2823681049bb48f44bf27603208
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-05-12 14:27:29 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -2,4 +2,16 @@ class TicTacToe
 	def initialize(board=nil)
 		@board = board || Array.new(9, " ")
 	end
+
+	WIN_COMBINATIONS = [
+  [0, 1, 2],
+  [0, 4, 8],
+  [0, 3, 6],
+  [3, 4, 5],
+  [6, 4, 2],
+  [1, 4, 7],
+  [6, 7, 8],
+  [2, 5, 8]
+]
+
 end
