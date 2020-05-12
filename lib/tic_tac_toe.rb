d6089bdecaab1c949cc83ad81ca608506a1d1643
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-05-12 14:46:25 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -32,14 +32,14 @@ class TicTacToe
 	end
 
 	def position_taken?(board, index)
-  if @board[index] == "" || @board[index] == " " || @board[index] == nil
-    false
-  elsif
-    @board[index] == "X" || @board[index] == "O"
-    true
-  else
-    nil
-  end
+  	if @board[index] == "" || @board[index] == " " || @board[index] == nil
+    	false
+  	elsif
+    	@board[index] == "X" || @board[index] == "O"
+    	true
+  	else
+    	nil
+  	end
 end
 
 end
