baa0be896d89c40b85e9e9ef735893fc117bb939
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-05-12 14:48:54 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -46,4 +46,16 @@ end
   !position_taken?(index) && index.between?(0,8)
 end
 
+	def turn(board)
+    puts "Please enter 1-9:"
+    input = gets.strip
+    index = input_to_index(input)
+    if valid_move?(board,index) == true
+    move(board, index, current_player(board))
+    display_board(board)
+  else
+    turn(board)
+  end
+end
+
 end
