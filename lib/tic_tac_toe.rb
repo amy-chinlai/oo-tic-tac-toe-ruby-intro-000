326b6cf03356beb57d6dbba9f36e032f7592b0eb
Amy Chin-Lai <amy.chinlai@gmail.com>
2020-05-12 14:54:20 +0000

    Automatically backed up by Learn

--- lib/tic_tac_toe.rb
+++ lib/tic_tac_toe.rb
@@ -48,11 +48,11 @@ end
 
 	def turn
     puts "Please enter 1-9:"
-    @input = gets.strip
-    @index = input_to_index(input)
+    input = gets.strip
+    index = input_to_index(input)
     if valid_move?(index) == true
-    move(board, index, current_player)
-    display_board
+    	move(index, current_player)
+    	display_board
   else
     turn
   end
